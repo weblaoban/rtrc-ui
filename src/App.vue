e8757e1b0c117dcf0ac00b99ed3cd4f0e92f1160
tong <904726194@qq.com>
2025-04-29 09:51:14 +0800

    数据联动

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
 	<div>
-		{{selected}}
-		<button @click="selected = ['2025-04-29']">11111</button>
 		<rtrc-select-date v-model="selected" multiply formatter="YYYY-MM-DD"></rtrc-select-date>
 	</div>
 </template>
@@ -10,6 +8,4 @@ import {ref} from "vue";
 // @ts-ignore
 import RtrcSelectDate from "../components/components/SelectDate/index.vue";
 const selected = ref([])
-
-selected.value = ['2025-04-29']
 </script>
